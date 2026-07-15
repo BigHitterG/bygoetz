d9631f1becf2d0dc681c624846e2aa1ef54a16c5
BigHitterG <thomas.goetz.jr@gmail.com>
2026-07-15 16:54:53 -0500

    Add Goetz favicon for gallery builder

---
+++ app/explorers/build-a-set/icon.tsx
@@ -0,0 +1,31 @@
+import { ImageResponse } from "next/og";
+
+export const size = { width: 512, height: 512 };
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          height: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+          background: "#fff",
+        }}
+      >
+        <img
+          alt="Goetz"
+          src="https://www.bygoetz.com/concepts/images/Logo-01.png"
+          width={440}
+          height={314}
+          style={{ objectFit: "contain" }}
+        />
+      </div>
+    ),
+    size,
+  );
+}
+
